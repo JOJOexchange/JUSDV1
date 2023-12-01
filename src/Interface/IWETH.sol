79fb364c51206da4e5ea4d2dbe4816666520ec6e
JoscelynFarr <fushikun307@outlook.com>
2023-12-01 15:32:36 +0800

    Revert "update"
    This reverts commit f442f718cf2c0c6db422f743521c33635755a788.

--- src/Interface/IWETH.sol
+++ src/Interface/IWETH.sol
@@ -4,10 +4,20 @@
 
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+interface IWETH {
+    function totalSupply() external view returns (uint256);
 
-interface IWETH is IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address src, address dst, uint256 wad) external returns (bool);
+
     function deposit() external payable;
 
-    function withdraw(uint256) external;
+    function withdraw(uint256 wad) external;
 }
